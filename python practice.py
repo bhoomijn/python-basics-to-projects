5dd2464ebd3bda3a5a2b1f84cf9eb9cd306be964
Bhoomi Jain <bhoomijn4@gmail.com>
2026-06-26 09:57:37 +0530

    Update python practice.py

--- python practice.py
+++ python practice.py
@@ -6,3 +6,17 @@ def avg():
     avg=(a+b)/2
     print(avg)
 avg()
+
+print("Hello, World!")
+
+def avg():
+    a=int(input("Enter the first number: "))
+    b=int(input("Enter a number: "))
+    avg=(a+b)/2
+    print(avg)
+avg()
+print("This is a simple program to calculate the average of two numbers.")
+print("Thank you for using this program!")
+avg()
+
+
