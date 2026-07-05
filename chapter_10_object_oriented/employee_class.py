2d5ae9b058b2f1153b817563cacc722d430bcb02
Bhoomi Jain <bhoomijn4@gmail.com>
2026-07-05 10:19:44 +0530

    Create employee_class.py

---
+++ chapter_10_object_oriented/employee_class.py
@@ -0,0 +1,16 @@
+# Chapter 10: Object Oriented Programming
+# Simple class and objects example
+
+class Employee:
+    name = "Harry"
+    language = "Python"
+
+# First object
+harry = Employee()
+harry.name = "Bhoomi"
+print(harry.name, harry.language)
+
+# Second object
+rohan = Employee()
+rohan.name = "Roha_n"
+print(rohan.name, rohan.language)
