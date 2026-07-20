5d0028a6c752c6aa6891f08399ea3c6992c8dbc9
Bhoomi Jain <bhoomijn4@gmail.com>
2026-07-20 20:29:10 +0530

    Update 03_problem3.py

--- oops-inheritance/03_problem3.py
+++ oops-inheritance/03_problem3.py
@@ -1,1 +1,19 @@
 
+# employee_salary_increment.py
+# Demonstrates use of @property decorator in Python
+
+class Employee:
+    salary = 2000
+    increment = 500
+
+    @property
+    def salary_after_increment(self):
+        return self.salary + self.salary * (self.increment) / 100
+
+
+# Object creation
+e = Employee()
+
+# Accessing property like an attribute
+print("Salary after increment:", e.salary_after_increment)
+# Output: Salary after increment: 12000
