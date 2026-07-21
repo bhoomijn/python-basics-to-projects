5220ef99bc4c9880d94848f87efe51d0dc676391
Bhoomi Jain <bhoomijn4@gmail.com>
2026-07-21 21:54:04 +0530

    Update 04_problem4.py

--- oops-inheritance/04_problem4.py
+++ oops-inheritance/04_problem4.py
@@ -1,1 +1,15 @@
 
+# question3 
+
+class complex:
+    def __init__(self,r,i):
+        self.r = r
+        self.i = i
+
+    def __add__(self,c2):
+        return complex(self.r + c1.r,self.i+c2.i)
+    
+c1 = complex(2,5)
+c2 = complex(6,9)
+print(c1+c2)
+    
