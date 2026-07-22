9fc442f30d5b3286e56a4f910fd38feca99a1568
Bhoomi Jain <bhoomijn4@gmail.com>
2026-07-22 19:08:06 +0530

    Update 04_problem4.py

--- oops-inheritance/04_problem4.py
+++ oops-inheritance/04_problem4.py
@@ -9,7 +9,10 @@ class complex:
     def __add__(self,c2):
         return complex(self.r + c1.r,self.i+c2.i)
     
-c1 = complex(2,5)
+    def __str__(self):
+        return f"{self.r} + {self.i}"
+    
+c1 = complex(1,5)
 c2 = complex(6,9)
 print(c1+c2)
     
