b6fc0dcf10dc881836d62327054d9c0892aec069
Bhoomi Jain <bhoomijn4@gmail.com>
2026-07-24 18:16:39 +0530

    Update 06_problem6.py

--- oops-inheritance/06_problem6.py
+++ oops-inheritance/06_problem6.py
@@ -1,1 +1,26 @@
 
+#question6
+
+class Vector:
+    def __init__(self,x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __add__(self,other):
+        result = self.x + other.x + self.y + other.y + self.z + other.z
+        return result
+
+    def __str__(self):
+        return f"({self.x}i + {self.y}j + {self.z}k)"
+
+v1 = Vector(3,7,8)
+v2 = Vector(4,7,9)
+v3 = Vector(2,4,5)
+
+print(v1+v2)
+
+print(v2+v3)
+
+print(v1+v3)
+print(v1+v3)
